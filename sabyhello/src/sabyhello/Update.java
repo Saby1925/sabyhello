158b713fb7df070868f2f9e827b741edb058396d
sdac11 <sdac11@sdac11-PC>
2023-06-15 14:40:03 +0530

    update 1.0

--- sabyhello/src/sabyhello/Update.java
+++ sabyhello/src/sabyhello/Update.java
@@ -1,7 +1,7 @@
 package sabyhello;
 
 public class Update {
-public static void main(String[] args) {
-	System.out.println("update 1.0");
-}
+	public static void main(String[] args) {
+		System.out.println("update 1.0");
+	}
 }
